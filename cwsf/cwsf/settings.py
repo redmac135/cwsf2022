cb672ded83f978fde6bd9f1b52b4918dd665ff92
bryan <bryan.deng002@gmail.com>
2022-06-28 12:12:36 -0400

    "whatever i have"

--- cwsf/cwsf/settings.py
+++ cwsf/cwsf/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'pages',
     'diagnose',
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'cwsf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
