fabfac5b9c02eed17fdf49123a17b854d6f59d3a
bryan <bryan.deng002@gmail.com>
2022-06-28 20:23:07 -0400

    made it work locally

--- cwsf/cwsf/settings/development.py
+++ cwsf/cwsf/settings/development.py
@@ -1,6 +1,6 @@
 from .defaults import *
 
-DEBUG = env.bool('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS=['https://localhost:8000','https://127.0.0.1:8000','http://localhost:8000','http://127.0.0.1:8000']
@@ -11,6 +11,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
+
+SECRET_KEY = "sz16O6Fz1Mx6TKqRHUtK5j1LpxdnJTmbwOS0hpajyjU="
 
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
