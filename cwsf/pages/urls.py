504625a85f0f6e85066f131065f22cf3740ca02e
bryan <bryan.deng002@gmail.com>
2022-06-28 22:23:08 -0400

    add kaggle

--- cwsf/pages/urls.py
+++ cwsf/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, NotebookPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('notebook/', NotebookPageView.as_view(), name='notebook'),
 ]
