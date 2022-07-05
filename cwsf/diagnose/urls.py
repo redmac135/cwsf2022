bd38f692c95604da54ba0d5c0ffdb26c94cb8b3e
redmac135 <ethanzhao1981@gmail.com>
2022-07-05 19:05:01 -0400

    refactor url patterns

--- cwsf/diagnose/urls.py
+++ cwsf/diagnose/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import DiagnoseView, GenelabView, valueError, downloadFile, exampleView
+from .views import DiagnoseView, GenelabView, valueError, downloadFile, diagnose_file, genelab_file
 
 urlpatterns = [
-    path("", DiagnoseView.as_view(), name="home"),
-    path("genelab", GenelabView.as_view(), name="genelab"),
-    path("example/<str:filename>", exampleView, name="exampleView"),
     path("error", valueError, name="valueError"),
+    path("diagnose", DiagnoseView.as_view(), name="diagnose"),
+    path("genelab", GenelabView.as_view(), name="genelab"),
+    path("diagnose/<str:filename>", diagnose_file, name="diagnose_file"),
+    path("genelab/<str:filename>", genelab_file, name="genelab_file"),
     path("download/<str:filename>", downloadFile, name="download"),
 ]
--- cwsf/diagnose/views.py
+++ cwsf/diagnose/views.py
@@ -13,9 +13,7 @@ from .utils import parse_file, geneNames, getRGBA, linear_color_map, plotCompari
 from .ai import predict
 
 
-# Create your views here.
-
-
+# Main Views
 class DiagnoseView(View):
     template_names = ["diagnose/diagnose_form.html", "diagnose/diagnose_results.html"]
     form_class = DiagnoseForm
@@ -59,56 +57,6 @@ class DiagnoseView(View):
                 }),
             },
         )
-
-examplenames = ["example2.txt", "example3.txt", "example4.txt"]
-def exampleView(request, filename:str = ""):
-    if filename == "":
-        return redirect('home')
-    if filename not in examplenames:
-        return redirect('home')
-    filepath = os.path.join(
-        BASE_DIR, "diagnose", "static", "diagnose", "files", filename
-    )
-    with open(filepath, "rb+") as f:
-        f.seek(0)
-        data = f.read().decode()
-        lines = re.split(r"[\s,]+", data)
-    results = list(map(float, lines))
-    preds = predict(results)
-    matrix_colors = linear_color_map(results).tolist()
-    labels = [x[0] for x in preds]
-    output = [x[1] for x in preds]
-    colors = [getRGBA(x) for x in output]
-    return render(
-        request,
-        "diagnose/diagnose_results.html",
-        {
-            "labels": labels,
-            "output": output,
-            "colors": colors,
-            "matrix_data": json.dumps({
-                "gene_names": geneNames,
-                "matrix_colors": matrix_colors,
-            }),
-        },
-    )
-
-def valueError(request):
-    return render(request, "diagnose/valueError.html", {})
-
-def downloadFile(request, filename=""):
-    if filename == "":
-        return redirect('home')
-    filepath = os.path.join(
-        BASE_DIR, "diagnose", "static", "diagnose", "files", filename
-    )
-    path = open(filepath, "rb")
-    mime_type, _ = mimetypes.guess_type(filepath)
-    response = HttpResponse(path, content_type=mime_type)
-    # Set the HTTP header for sending to browser
-    response["Content-Disposition"] = f"attachment; filename={filename}"
-
-    return response
 
 class GenelabView(View):
     template_names = ["diagnose/genelab_form.html", "diagnose/genelab_results.html"]
@@ -158,3 +106,94 @@ class GenelabView(View):
                 }),
             },
         )
+
+# Example Views
+EXAMPLE_NAMES = ["example2.txt", "example3.txt", "example4.txt"]
+
+def diagnose_file(request, filename:str = ""):
+    if filename == "":
+        return redirect('diagnose')
+    if filename not in EXAMPLE_NAMES:
+        return redirect('diagnose')
+
+    filepath = os.path.join(
+        BASE_DIR, "diagnose", "static", "diagnose", "files", filename
+    )
+    with open(filepath, "rb+") as f:
+        f.seek(0)
+        data = f.read().decode()
+        lines = re.split(r"[\s,]+", data)
+    results = list(map(float, lines))
+    preds = predict(results)
+    matrix_colors = linear_color_map(results).tolist()
+    labels = [x[0] for x in preds]
+    output = [x[1] for x in preds]
+    colors = [getRGBA(x) for x in output]
+    return render(
+        request,
+        DiagnoseView.template_names[1],
+        {
+            "labels": labels,
+            "output": output,
+            "colors": colors,
+            "matrix_data": json.dumps({
+                "gene_names": geneNames,
+                "matrix_colors": matrix_colors,
+            }),
+        },
+    )
+
+def genelab_file(request, filename:str = ""):
+    if filename == "":
+        return redirect('genelab')
+    if filename not in EXAMPLE_NAMES:
+        return redirect('genelab')
+
+    filepath = os.path.join(
+        BASE_DIR, "diagnose", "static", "diagnose", "files", filename
+    )
+    with open(filepath, "rb+") as f:
+        f.seek(0)
+        data = f.read().decode()
+        lines = re.split(r"[\s,]+", data)
+    results = list(map(float, lines))
+    log_samples, log_control, comp_colors = plotComparison(results)
+    log2FC, p, volcano_colors = plotVolcano(results)
+
+    comp = [{'x': log_samples[i], 'y': log_control[i]} for i in range(len(log_samples))]
+
+    volcano = [{'x': log2FC[i], 'y': p[i]} for i in range(len(log2FC))]
+
+    matrix_colors = linear_color_map(results).tolist()
+    return render(
+        request,
+        GenelabView.template_names[1],
+        {
+            "comp": comp,
+            "comp_colors": comp_colors,
+            "volcano": volcano,
+            "volcano_colors": volcano_colors,
+            "matrix_data": json.dumps({
+                "gene_names": geneNames,
+                "matrix_colors": matrix_colors,
+            }),
+        },
+    )
+
+# Utility Views
+def valueError(request):
+    return render(request, "diagnose/valueError.html", {})
+
+def downloadFile(request, filename=""):
+    if filename == "":
+        return redirect('home')
+    filepath = os.path.join(
+        BASE_DIR, "diagnose", "static", "diagnose", "files", filename
+    )
+    path = open(filepath, "rb")
+    mime_type, _ = mimetypes.guess_type(filepath)
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response["Content-Disposition"] = f"attachment; filename={filename}"
+
+    return response
